4cfc8928e74fced893ff044693368f4cb541e68d
Kang Shentu <shentu.k@northeastern.edu>
2022-03-10 00:07:49 -0500

    a5

--- assignment-functional-composition/src/main/scala/edu/neu/coe/csye7200/asstfc/Movie.scala
+++ assignment-functional-composition/src/main/scala/edu/neu/coe/csye7200/asstfc/Movie.scala
@@ -102,7 +102,13 @@ object Movie extends App {
   object MoviesProtocol extends DefaultJsonProtocol {
     // 20 points
     // TO BE IMPLEMENTED
-    ???
+    implicit val formatFormat: RootJsonFormat[Format] = jsonFormat4(Format.apply)
+    implicit val productionFormat: RootJsonFormat[Production] = jsonFormat4(Production.apply)
+    implicit val reviewJsonFormat: RootJsonFormat[Reviews] = jsonFormat7(Reviews.apply)
+    implicit val principalFormat: RootJsonFormat[Principal] = jsonFormat2(Principal.apply)
+    implicit val nameFormat: RootJsonFormat[Name] = jsonFormat4(Name.apply)
+    implicit val ratingFormat: RootJsonFormat[Rating] = jsonFormat2(Rating.apply)
+    implicit val movieJsonFormat: RootJsonFormat[Movie] = jsonFormat11(Movie.apply)
   }
 
   implicit object IngestibleMovie extends IngestibleMovie
@@ -120,10 +126,13 @@ object Movie extends App {
   }
 
   //Hint: Serialize the input to Json format and deserialize back to Object, check the result is still equal to original input.
+
+  import MoviesProtocol._
+
   def testSerializationAndDeserialization(ms: Seq[Movie]): Boolean = {
     // 5 points
     // TO BE IMPLEMENTED
-    ???
+    ms.toJson.convertTo[Seq[Movie]] == ms
   }
 
   def getMoviesFromCountry(country: String, movies: Iterator[Try[Movie]]): Try[Seq[Movie]] = {
